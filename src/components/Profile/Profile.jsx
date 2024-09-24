2e693e584f2996fafb6f163842a8a0e9847b7d50
Eduardo Montenegro <edomontt.sepeda@gmail.com>
2024-09-24 18:18:12 -0300

    Commit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,51 @@
 import { Typography } from '@mui/material';
 
-import CustomTimeline from '@/components/Timeline/Timeline';
+import { TimelineItem, TimelineContent } from '@mui/lab';
+
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
+import CustomTimeline, { CustomTimelineSeparator } from '@/components/Timeline/Timeline';
 
 import '@/components/Profile/Profile.css';
+
+import data from '@/utils/data';
+
+const CustomTimelineItem = ({ title, text, link }) =>
+{
+    return (
+        <TimelineItem>
+            <CustomTimelineSeparator />
+            <TimelineContent className="timeline_content">
+
+                {
+                    link ?
+                    (
+                        <Typography className="timelineItem_text">
+                            <span>{title}:</span>{' '}
+                            <a href={link} target="_blank">
+                                {text}
+                            </a>
+                        </Typography>
+                    ) :
+                    (
+                        <Typography className="timelineItem_text">
+                            <span>{title}:</span> {text}
+                        </Typography>
+                    )
+                }
+
+            </TimelineContent>
+        </TimelineItem>
+    );
+}
 
 const Profile = () =>
 {
     return (
         <div className="profile container_shadow">
             <div className="profile_name">
-                <Typography className="name">Sam</Typography>
-                <Typography className="title">Title</Typography>
+                <Typography className="name">{data.name}</Typography>
+                <Typography className="title">{data.title}</Typography>
             </div>
 
             <figure className='profile_image'>
@@ -18,7 +53,19 @@ const Profile = () =>
             </figure>
 
             <div className="profile_information">
-                <CustomTimeline />
+                <CustomTimeline icon={<PersonOutlineIcon />}>
+                    <CustomTimelineItem title="Name" text={data.name} />
+                    <CustomTimelineItem title="Title" text={data.title} />
+                    <CustomTimelineItem title="Email" text={data.email} />
+
+                    {
+                        Object.keys(data.socials).map((key) =>
+                        (
+                            <CustomTimelineItem title={key} text={data.socials[key].text} link={data.socials[key].link} />
+                        ))
+                    }
+
+                </CustomTimeline>
                 <br />
                 <button>my Button</button>
             </div>
---
+++ src/utils/data.js
@@ -0,0 +1,43 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+export default
+{
+    name: 'Sam is coding',
+    title: 'Full stack developer',
+
+    birthday: '06th December 1999',
+    email: 'sam@sam.com',
+    address: '123 Test St',
+    phone: '123 456 7890',
+
+    socials:
+    {
+        Facebook:
+        {
+            link: 'https://www.facebook.com',
+            text: 'SamIsCoding',
+            icon: <FacebookIcon />
+        },
+        Twitter:
+        {
+            link: 'https://www.facebook.com',
+            text: 'MyTwitter',
+            icon: <TwitterIcon />
+        },
+        LinkedIn:
+        {
+            link: 'https://www.facebook.com',
+            text: 'MyLinkedIn',
+            icon: <LinkedInIcon />
+        },
+        GitHub:
+        {
+            link: 'https://www.facebook.com',
+            text: 'MyGithub',
+            icon: <GitHubIcon />
+        }
+    }
+}
